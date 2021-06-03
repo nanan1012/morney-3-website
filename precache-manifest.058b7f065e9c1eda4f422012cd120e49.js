4cc1b91421f22b0e903ed597dc6109e2f9ccd3f0
nanan <1074274996@qq.com>
2021-06-03 23:41:36 +0800

    deploy

---
+++ precache-manifest.058b7f065e9c1eda4f422012cd120e49.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed659b59ef87cf2d6f62",
+    "url": "/morney-3-website/css/app.fb768308.css"
+  },
+  {
+    "revision": "574c04481b74e55aab1644ac7e241bc9",
+    "url": "/morney-3-website/index.html"
+  },
+  {
+    "revision": "ed659b59ef87cf2d6f62",
+    "url": "/morney-3-website/js/app.59ccdd22.js"
+  },
+  {
+    "revision": "1979d97306db5ea62bf5",
+    "url": "/morney-3-website/js/chunk-vendors.12b9dfde.js"
+  },
+  {
+    "revision": "d1372d61b03359d367fb319fe20d26c9",
+    "url": "/morney-3-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/morney-3-website/robots.txt"
+  }
+]);
